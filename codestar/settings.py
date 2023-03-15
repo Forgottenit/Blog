527215df05592b1fa680894456246869cd1c9c40
Forgottenit <forgottenit@hotmail.com>
2023-03-15 19:15:00 +0000

    Testing Debug status

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get("DEVELOPMENT")
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["forgottenit-codestar-blog.herokuapp.com", "localhost"]
 
 # Application definition
